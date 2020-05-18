7046d0da50a7e5bedc290aad674e9324a33c7246
kim_svadoz <dhkdghehfdl@gmail.com>
2020-05-18 17:50:46 +0900

    miri

--- driving/driving.ino
+++ driving/driving.ino
@@ -2,10 +2,10 @@
    Smart Robot Car V3
    - Motor test code
 */
-#include <IRremote.h>
+#include "NewPing.h"
 // 차량 운행 방향 상태 전역 변수.
 
-int carSpeed = 200; // 최대 속도의  50 % for testing.
+int carSpeed = 194; // 최대 속도의  50 % for testing.
 
 #define ENA   11
 #define EN1   7 // 왼쪽 뒤로
@@ -13,10 +13,12 @@ int carSpeed = 200; // 최대 속도의  50 % for testing.
 #define EN3   4 // 오른쪽 앞으로
 #define EN4   2 // 오른쪽 뒤로
 #define ENB   5
-const int RECV_PIN = 10;
-IRrecv irrecv(RECV_PIN);
-decode_results results;
 
+#define ECHO   8
+#define TRIGGER   9
+
+NewPing sonar(ECHO, TRIGGER, 200);
+const int RECV_PIN = 10;
 
 //////////////////////////////////
 // Setup to run once
@@ -31,32 +33,47 @@ void setup()
   pinMode(ENB, OUTPUT);  // ENB
   pinMode(EN3, OUTPUT);  // EN3
   pinMode(EN4, OUTPUT);  // EN4
-  irrecv.enableIRIn();
-  irrecv.blink13(true);
-
-
-
-
 }
 void loop()
-{/*
-  if (Serial.available() > 1) {
-    char a = Serial.read();
+{
+  Serial.print("Ping : ");
+  Serial.print(sonar.ping_cm());
+  Serial.println("cm");
+  delay(1000);
+  
+  if (Serial.available() > 0) {
+    int a = Serial.read();
     switch (a) {
-      case 'g':
-        go(carSpeed);
+      case 'f':
+        forward(carSpeed);
+        break;
+      case 'x':
+        left(carSpeed);
+        break;
+      case 'y':
+        right(carSpeed);
         break;
       case 'b':
         back(carSpeed);
         break;
-      case 'r':
-        right(carSpeed);
-        break;
       case 'l':
-        left(carSpeed);
+        leftspin();
+        break;
+      case 'r':
+        rightspin();
+        break;
+      case 'u':
+        up(carSpeed);
+        break;
+      case 'd':
+        down(carSpeed);
+        break;
+      case 's':
+        stopD();
         break;
     }
-    */
+    
+    /*
     if (irrecv.decode(&results)){
     if(results.value==16718055){
       go(carSpeed);
@@ -72,8 +89,10 @@ void loop()
     }
     irrecv.resume();
   }
+  */
+  }
 }
-void go(int speed)
+void forward(int speed)
 {
   analogWrite(ENA, speed);
   analogWrite(ENB, speed);
@@ -81,9 +100,26 @@ void go(int speed)
   digitalWrite(EN2, HIGH);
   digitalWrite(EN3, HIGH);
   digitalWrite(EN4, LOW);
-  delay(1000);
+}
+void left(int speed)
+{
   analogWrite(ENA, 0);
+  analogWrite(ENB, speed);
+  digitalWrite(EN1, HIGH);
+  digitalWrite(EN2, LOW);
+  digitalWrite(EN3, HIGH);
+  digitalWrite(EN4, LOW);
+}
+void right(int speed)
+{
+  int mSpeed = speed + 20;
+  if(mSpeed >= 255) mSpeed = 255;
+  analogWrite(ENA, speed);
   analogWrite(ENB, 0);
+  digitalWrite(EN1, LOW);
+  digitalWrite(EN2, HIGH);
+  digitalWrite(EN3, LOW);
+  digitalWrite(EN4, HIGH);
 }
 void back(int speed)
 {
@@ -93,29 +129,38 @@ void back(int speed)
   digitalWrite(EN2, LOW);
   digitalWrite(EN3, LOW);
   digitalWrite(EN4, HIGH);
-  delay(1000);
-  analogWrite(ENA, 0);
-  analogWrite(ENB, 0);
 }
-void right(int speed) {
-  analogWrite(ENA, speed);
-  analogWrite(ENB, speed);
-  digitalWrite(EN1, LOW);
-  digitalWrite(EN2, HIGH);
-  digitalWrite(EN3, LOW);
-  digitalWrite(EN4, HIGH);
-  delay(1000);
-  analogWrite(ENA, 0);
-  analogWrite(ENB, 0);
+void up(int speed) {
+  carSpeed += 20;
+  if(carSpeed>=255) carSpeed=255;
 }
-void left(int speed) {
-  analogWrite(ENA, speed);
-  analogWrite(ENB, speed);
+void down(int speed) {
+  carSpeed -= 20;
+  if(carSpeed<=100) carSpeed=100;
+}
+void leftspin() {
+  analogWrite(ENA, 120);
+  analogWrite(ENB, 120);
   digitalWrite(EN1, HIGH);
   digitalWrite(EN2, LOW);
   digitalWrite(EN3, HIGH);
   digitalWrite(EN4, LOW);
-  delay(1000);
+  delay(500);
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
+}
+void rightspin() {
+  analogWrite(ENA, 120);
+  analogWrite(ENB, 120);
+  digitalWrite(EN1, LOW);
+  digitalWrite(EN2, HIGH);
+  digitalWrite(EN3, LOW);
+  digitalWrite(EN4, HIGH);
+  delay(500);
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
+}
+void stopD(){
   analogWrite(ENA, 0);
   analogWrite(ENB, 0);
 }
---
+++ serial_output/serial_output.ino
@@ -0,0 +1,9 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("hello");
+  Serial.flush();
+  delay(1000);
+}
